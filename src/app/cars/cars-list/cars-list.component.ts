b069c0954e5f1dca291b37731badc5910667871f
Dariusz Sibik <sibikdariusz@gmail.com>
2018-11-25 21:53:32 +0100

    add @ViewChild and ngAfterViewInit

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewEncapsulation, ViewChild, AfterViewChecked } from '@angular/core';
 import { Car } from '../models/car';
+import { TotalCostComponent } from '../total-cost/total-cost.component';
 
 @Component({
   selector: 'cs-cars-list',
@@ -7,7 +8,8 @@ import { Car } from '../models/car';
   styleUrls: ['./cars-list.component.less'],
   encapsulation: ViewEncapsulation.None
 })
-export class CarsListComponent implements OnInit {
+export class CarsListComponent implements OnInit, AfterViewInit {
+  @ViewChild("totalCostRef") totalCostRef : TotalCostComponent
   totalCost : number
   grossCost : number
   cars: Car[] = [
@@ -56,6 +58,14 @@ export class CarsListComponent implements OnInit {
 
   ngOnInit() {
       this.countTotalCost();
+  }
+
+  ngAfterViewInit() {
+      this.totalCostRef.showGross();
+  }
+
+  showGross() : void {
+      this.totalCostRef.showGross();
   }
 
   countTotalCost() : void {
