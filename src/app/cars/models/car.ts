e774776b2524c5cc13bac6762e7039f2922fb632
Dariusz Sibik <sibikdariusz@gmail.com>
2018-12-09 17:35:02 +0100

    update Forms

--- src/app/cars/models/car.ts
+++ src/app/cars/models/car.ts
@@ -2,14 +2,14 @@ export interface Car {
         id: number;
         model: string;
         type: string;
+        year: number;
+        color: string;
+        cost: number;
+        isFullyDamaged: boolean;
+        clientFirstName: string;
+        clientSurname: string;
+        power: number;
         plate: string;
         deliveryDate: string;
         deadline: string;
-        color: string;
-        power: number;
-        clientFirstName: string;
-        clientSurname: string;
-        cost: number;
-        isFullyDamaged: boolean;
-        year: number;
 }
