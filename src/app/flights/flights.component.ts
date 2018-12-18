97d86747af263b860726ce78aba8ad960b55c37e
Dariusz Sibik <sibikdariusz@gmail.com>
2018-12-18 23:09:15 +0100

    add flights service

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlightsService } from './flights.service';
 
 @Component({
   selector: 'cs-flights',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private flightsService: FlightsService ) { }
 
   ngOnInit() {
+    this.flightsService.getFlights().subscribe(console.log);
   }
 
 }
