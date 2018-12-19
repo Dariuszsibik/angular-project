8d28e8f2ef199eac3edf185388fbae3cb5d1b982
Dariusz Sibik <sibikdariusz@gmail.com>
2018-12-19 23:12:51 +0100

    add global config modal

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,12 @@
+
 import { NgModule } from '@angular/core';
-import { MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule, MatDialogModule, MatButtonModule, MatSnackBarModule, MatCheckboxModule, MatSelectModule, MatToolbarModule } from '@angular/material';
+import { MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule, MatDialogModule, MatButtonModule, MatSnackBarModule, MatCheckboxModule, MatSelectModule, MatToolbarModule, MatDialogConfig, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material';
+
+const MAT_DIALOG_GLOBAL_CONFIG: MatDialogConfig = {
+  width: '700px',
+  disableClose: true,
+  hasBackdrop: true
+}
 
 const MATERIAL_MODULES = [
   MatIconModule,
@@ -13,8 +20,12 @@ const MATERIAL_MODULES = [
   MatSelectModule,
   MatToolbarModule
 ];
+
 @NgModule({
   declarations: [],
-  exports: [...MATERIAL_MODULES]
+  exports: [...MATERIAL_MODULES],
+  providers: [
+    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: MAT_DIALOG_GLOBAL_CONFIG }
+  ]
 })
 export class MaterialModule { }
