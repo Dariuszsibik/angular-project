1ec39eb7ed99e0f4f07a838dab2ea3425cd81195
Dariusz Sibik <dariusz.sibik@uj.edu.pl>
2018-12-20 12:00:20 +0100

    connect fight-form template with flight-form class

--- src/app/flights/flight-form/flight-form.component.ts
+++ src/app/flights/flight-form/flight-form.component.ts
@@ -1,5 +1,5 @@
 import * as core from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @core.Component({
   selector: 'cs-flight-form',
@@ -7,13 +7,38 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./flight-form.component.less']
 })
 export class FlightFormComponent implements core.OnInit {
-
   form: FormGroup;
+  jobs = [
+    { label: 'Stwaredess', value: 'stweradess'},
+    { label: 'Senior Cabin Crew', value: 'senior_cabin_crew'},
+    { label: 'Pilot', value: 'pilot'},
+    { label: 'Co-Pilot', value: 'co_pilot'},
+    { label: 'Mechanic', value: 'mechanic'},
+  ];
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.buildForm();
+  }
+
+  get crew() {
+    return this.form.get('crew') as FormArray;
+  }
+
+  removeCrewMember(i: number) {
+    this.crew.removeAt(i);
+  }
+
+  addCrewMember() {
+    this.crew.push(this.buildCrewMember());
+  }
+
+  buildCrewMember() {
+    return this.formBuilder.group({
+      name: '',
+      job: ''
+    });
   }
 
   private buildForm() {
@@ -24,7 +49,8 @@ export class FlightFormComponent implements core.OnInit {
       returnTime: '',
       code: '',
       additionalInformation: '',
-      withSKPlanesDiscount: false
+      withSKPlanesDiscount: false,
+      crew: this.formBuilder.array([this.buildCrewMember()])
     });
   }
 }
