38e982947b9cd71d1da03d58c73680a79029f89c
Dariusz Sibik <dariusz.sibik@uj.edu.pl>
2018-12-20 15:40:16 +0100

    add edit-flight component

--- src/app/flights/details/details.component.ts
+++ src/app/flights/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { Flight } from '../model/flight.model';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cs-details',
@@ -11,13 +12,19 @@ export class DetailsComponent {
   flight: Flight;
 
   constructor(
+    private router: Router,
     private dialogRef: MatDialogRef<DetailsComponent>,
     @Inject(MAT_DIALOG_DATA) private data: Flight
   ) { this.flight = data; }
 
-close() {
-  this.dialogRef.close();
-}
+  goToEditFlight() {
+    this.dialogRef.close();
+    this.router.navigate( ['/flights', this.flight.key]);
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
 
 
 }
---
+++ src/app/flights/edit-flight/edit-flight.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cs-edit-flight',
+  templateUrl: './edit-flight.component.html',
+  styleUrls: ['./edit-flight.component.less']
+})
+export class EditFlightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
