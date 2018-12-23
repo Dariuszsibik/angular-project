af762d7194ab63fe820a7b88bba495ec00f65010
Dariusz Sibik <sibikdariusz@gmail.com>
2018-12-23 22:19:46 +0100

    add strategy OnPush

--- src/app/flights/flight-card/flight-card.component.ts
+++ src/app/flights/flight-card/flight-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Flight } from '../model/flight.model';
 
 @Component({
   selector: 'cs-flight-card',
   templateUrl: './flight-card.component.html',
-  styleUrls: ['./flight-card.component.less']
+  styleUrls: ['./flight-card.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightCardComponent {
   @Input() flight: Flight;
